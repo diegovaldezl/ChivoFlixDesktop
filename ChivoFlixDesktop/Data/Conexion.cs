bcf4caad293bf3b45d353ab09ad0c40654cb8a93
diegovaldezl <2547442017@mail.utec.edu.sv>
2022-04-11 20:09:36 -0600

    toques
    toques

--- ChivoFlixDesktop/Data/Conexion.cs
+++ ChivoFlixDesktop/Data/Conexion.cs
@@ -11,7 +11,7 @@ namespace ChivoFlixDesktop
 {
     public class Conexion
     {
-        public void crearBD()
+        public void CrearBD()
         {
             string database = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=master; Integrated Security=True;";
             string squery = "CREATE DATABASE CHIVOFLIX";
